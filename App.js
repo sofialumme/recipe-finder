00c1b5d2ee075de28620fb4ddf83a54f82f1d6f3
Sofia Lumme <sofia.lumme@gmail.com>
2023-02-13 21:49:02 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [ingredient, setIngredient] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = (ingredient) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+      .then(response => response.json())
+      .then(data => setRecipes(data.meals))
+      .catch(error => {
+        Alert.alert('Error', error);
+      });
+    console.log(recipes);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={ingredient => setIngredient(ingredient)}
+        value={ingredient}
+      />
+      <Button title='Find' onPress={() => getRecipes(ingredient)} />
+      <FlatList
+        data={recipes}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <View>
+            <Text>{item.strMeal}</Text>
+            <Image
+              style={styles.image}
+              source={{
+                uri: item.strMealThumb
+              }}
+            />
+            <Text> </Text>
+          </View>
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10
+  },
+  image: {
+    width: 200,
+    height: 200
+  }
 });
